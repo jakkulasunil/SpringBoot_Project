622c2bbd7122c6294dc30620a13094de4c572277
suniljakkula <sunij8911@gmail.com>
2023-05-01 15:12:49 +0530

    added yml file

--- src/main/java/com/spring/boot/controller/StudentController.java
+++ src/main/java/com/spring/boot/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.spring.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-	
+
+	Logger logger = LoggerFactory.getLogger(StudentController.class);
+
 	@GetMapping("/hello")
 	public String getMessage() {
+		logger.info("message printed successfully.");
 		return "Welcome to spring boot project.";
 	}
 
