8cf8e334340c4a4a35daef59c5d22a1d10f4269e
suniljakkula <sunij8911@gmail.com>
2023-05-03 07:58:37 +0530

    controller tests added

--- src/test/java/com/spring/boot/SpringBootProjectApplicationTests.java
+++ src/test/java/com/spring/boot/SpringBootProjectApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootProjectApplicationTests {
 
+	
 	@Test
 	void contextLoads() {
 	}
---
+++ src/test/java/com/spring/boot/StudentControllerTest.java
@@ -0,0 +1,38 @@
+package com.spring.boot;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.spring.boot.entity.Student;
+
+@WebMvcTest
+public class StudentControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	private Student student;
+
+	@BeforeEach
+	void init() {
+		student = new Student();
+		student.setId(1);
+		student.setName("Sunil");
+		student.setEmail("suni@gmail.com");
+		student.setAge(32);
+	}
+
+	@Test
+	public void getAllStudentDetails() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/allStudents").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk());
+	}
+}
