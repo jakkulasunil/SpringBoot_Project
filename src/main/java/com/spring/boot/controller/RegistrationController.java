1b4b3b097700fcdb72e83258a319083f04b8da60
suniljakkula <sunij8911@gmail.com>
2023-05-09 06:51:01 +0530

    saved data into Database

--- src/main/java/com/spring/boot/controller/RegistrationController.java
+++ src/main/java/com/spring/boot/controller/RegistrationController.java
@@ -1,5 +1,34 @@
 package com.spring.boot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.entity.Registration;
+import com.spring.boot.service.RegistrationService;
+
+@RestController
+@RequestMapping("/reg")
 public class RegistrationController {
+
+	@Autowired
+	private RegistrationService registrationService;
+
+	@GetMapping("/allDetails")
+	public List<Registration> getAllRegistrations() {
+		return registrationService.getAllRegistrations();
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<Registration> createRegistrations(@RequestBody Registration registration) {
+		return new ResponseEntity<Registration>(registrationService.createRegistrations(registration), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/spring/boot/service/RegistrationService.java
+++ src/main/java/com/spring/boot/service/RegistrationService.java
@@ -1,10 +1,29 @@
 package com.spring.boot.service;
 
-import com.spring.boot.dao.RegistrationDao;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.boot.dao.RegistrationDao;
+import com.spring.boot.entity.Registration;
+
+@Service
 public class RegistrationService {
 	
+	@Autowired
 	private RegistrationDao registrationDao;
+	
+	public Registration createRegistrations(Registration registration) {
+		return registrationDao.save(registration);
+	}
+	
+	@GetMapping("/allDetails")
+	public List<Registration> getAllRegistrations(){
+		
+		return (List<Registration>) registrationDao.findAll();
+	}
 	
 
 }
